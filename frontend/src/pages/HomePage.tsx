2fb6a73896268b44f3920bd4426590354427fb06
Ida Suominen <fidsu001@edu.xamk.fi>
2026-09-06 14:21:01 +0300

    S1-10 Router and placeholder home added

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    return(
+        <div>
+        <h1>Exercise Tracker App</h1>
+        <h2>This is an app for tracking your exercise.</h2>
+        <p>Sprint 1 placeholder.</p>
+        </div>
+    )
+}
+
+export default HomePage;
